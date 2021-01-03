eb4822cde20262231eec0788f25ee6298ee581a7
Dyego Aurelo <dyegoaurelio@gmail.com>
2021-01-03 15:21:30 -0300

    fix codium and node install

--- install.sh
+++ install.sh
@@ -89,10 +89,14 @@ sudo chmod -R o+rw,g+rw /usr/share/fonts/kingsoft
 sudo fc-cache -vfs
 
 # install vscodium 
-wget -qO - [https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg](https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg) | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg] [https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/](https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/) vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update
-sudo apt install codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt -y update
+sudo apt install -y codium
 #codium extensions
 codium --install-extension esbenp.prettier-vscode
 codium --install-extension yzhang.markdown-all-in-one
@@ -101,10 +105,10 @@ codium --install-extension Equinusocio.vsc-material-theme-icons
 codium --install-extension Equinusocio.vsc-material-theme
 
 #install programming languages
-sudo apt install python3 python3-pip
+sudo apt install -y python3 python3-pip
 pip install jupyter notebook
 
-sudo apt install nodejs npm
+sudo apt install -y nodejs npm
 
 #TO DO
 #config firefox
